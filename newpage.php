1df3cf5cb92d5886a7214d8db6a325b917c1f4d3
Ken Vandivort <kenvandivort@hotmail.com>
2018-09-17 19:41:31 -0500

    Merge 9.17.18

--- newpage.php
+++ newpage.php
@@ -1,79 +1,82 @@
 <?php
 /*
- Template Name: NEWPAGE
- *
- * This is your custom page template. You can create as many of these as you need.
- * Simply name is "page-whatever.php" and in add the "Template Name" title at the
- * top, the same way it is here.
- *
- * When you create your page, you can just select the template and viola, you have
- * a custom page template to call your very own. Your mother would be so proud.
- *
- * For more info: http://codex.wordpress.org/Page_Templates
+Template Name: NEWPAGE
+*
+* This is your custom page template. You can create as many of these as you need.
+* Simply name is "page-whatever.php" and in add the "Template Name" title at the
+* top, the same way it is here.
+*
+* When you create your page, you can just select the template and viola, you have
+* a custom page template to call your very own. Your mother would be so proud.
+*
+* For more info: http://codex.wordpress.org/Page_Templates
 */
 ?>
 <?php get_header(); ?>
 
-			<div id="content">
+<div id="content">
 
-				<div id="inner-content" class="wrap cf">
-
-
-
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div id="inner-content" class="wrap cf">
 
 
 
-								<header class="article-header">
-
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 
 
-								</header> <?php // end article header ?>
+<header class="article-header">
 
-								<section class="entry-content cf" itemprop="articleBody">
-									<?php
-										// the content (pretty self explanatory huh)
-										the_content();
-
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
-								</section> <?php // end article section ?>
-
-								<footer class="article-footer cf">
-
-								</footer>
-
-								<?php comments_template(); ?>
-
-							</article>
-
-							<?php endwhile; endif; ?>
-
-						</main>
+<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
 
 
 
-				</div>
+</header> <?php // end article header ?>
 
-			</div>
+<section class="entry-content cf" itemprop="articleBody">
+<?php
+// the content (pretty self explanatory huh)
+the_content();
+
+/*
+* Link Pages is used in case you have posts that are set to break into
+* multiple pages. You can remove this if you don't plan on doing that.
+*
+* Also, breaking content up into multiple pages is a horrible experience,
+* so don't do it. While there are SOME edge cases where this is useful, it's
+* mostly used for people to get more ad views. It's up to you but if you want
+* to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
+*
+* http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
+*
+*/
+wp_link_pages( array(
+'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
+'after' => '</div>',
+'link_before' => '<span>',
+'link_after' => '</span>',
+) );
+?>
+</section> <?php // end article section ?>
+
+<footer class="article-footer cf">
+
+</footer>
+
+<?php comments_template(); ?>
+
+</article>
+
+<?php endwhile; endif; ?>
+
+</main>
+
+
+
+</div>
+
+</div>
 
 <?php get_footer(); ?>
+
+
+Message
